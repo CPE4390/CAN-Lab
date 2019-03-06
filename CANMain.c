8c30d7fef9f6ba7f4dcc5d8c10856c23d332f2ea
EET4340 <45945733+EET4340@users.noreply.github.com>
2019-03-06 10:04:11 -0700

    Organized and commented CAN config section

--- CANMain.c
+++ CANMain.c
@@ -55,50 +55,49 @@ void ConfigPins(void) {
 void ConfigCAN(void) {
     TRISBbits.TRISB3 = 1; //CANRX must be input
     TRISBbits.TRISB2 = 0; //CANTX is output
-    //Switch to config mode
+    //Switch to config mode and wait for it
     CANCONbits.REQOP = 4;
     while (CANSTATbits.OPMODE != 4);
-
+    
+    //Now we're in config mode
     ECANCONbits.MDSEL = 0b00; //Legacy mode
-
-    //Set baud 125 Kbps
+    //Set baud to 125 Kbps
     BRGCON1bits.BRP = 15; //TQ = 1us
     BRGCON1bits.SJW = 0;
-    BRGCON2bits.SEG2PHTS = 1;
+    BRGCON2bits.SEG2PHTS = 1; //Don't assign phase 2 automatically
     BRGCON2bits.SEG1PH = 3;  //Phase 1 = 4TQ
     BRGCON2bits.PRSEG = 0; //Propagation = 1 TQ 
     BRGCON3bits.SEG2PH = 1; //Phase 2 = 2 TQ
 
-    //Set up Masks
-    //Mask0 used by RXBO
-    RXM0SIDH = 0b00000000;
-    RXM0SIDLbits.SID = 0b000; //Accept all id's to RXB0
-    
-    //Mask1 used by RXB1
-    RXM1SIDH = 0b11111111;
-    RXM1SIDLbits.SID = 0b111;
-
-    //Set up filters
-    RXF0SIDLbits.EXIDEN = RXF1SIDLbits.EXIDEN = RXF2SIDLbits.EXIDEN
-            = RXF3SIDLbits.EXIDEN = RXF4SIDLbits.EXIDEN = RXF5SIDLbits.EXIDEN
-            = 0; //Only accept standard messages in all filters
-    
+    //Set up Mask and filters for RB0
+    RXM0SIDH = 0b00000000;  //Mask0
+    RXM0SIDLbits.SID = 0b000; //Set mask to 0 to accept all id's to RXB0
+    //Only accept standard messages in filters
+    RXF0SIDLbits.EXIDEN = RXF1SIDLbits.EXIDEN = 0; 
     //Set F0 and F1 to accept 0x00
     //These filters are used by RXB0
-    RXF0SIDH = 0;
+    RXF0SIDH = 0b00000000;
     RXF0SIDLbits.SID = 0b000;
-    RXF1SIDH = 0;
+    RXF1SIDH = 0b00000000;
     RXF1SIDLbits.SID = 0b000;
     
-    
+    //Set up Mask and filters for RB1
+    RXM1SIDH = 0b11111111;
+    RXM1SIDLbits.SID = 0b111;  //Mask is set to all ones so filter must match
+    RXF2SIDLbits.EXIDEN = RXF3SIDLbits.EXIDEN = RXF4SIDLbits.EXIDEN 
+            = RXF5SIDLbits.EXIDEN = 0; //Only accept standard messages in all filters
     //F2 - F5 accept 0x00
-    RXF2SIDH = RXF3SIDH = RXF4SIDH = RXF5SIDH = 0;
-    RXF2SIDLbits.SID = RXF3SIDLbits.SID = RXF4SIDLbits.SID
-            = RXF5SIDLbits.SID = 0b000;
+    RXF2SIDH = 0b00000000;
+    RXF2SIDLbits.SID = 0b000;
+    RXF3SIDH = 0b00000000;
+    RXF3SIDLbits.SID = 0b000;
+    RXF4SIDH = 0b00000000;
+    RXF4SIDLbits.SID = 0b000;
+    RXF5SIDH = 0b00000000;
+    RXF5SIDLbits.SID = 0b000;
     
-    //Set up RXB0
+    //Set up buffers with filters
     RXB0CONbits.RXM1 = RXB0CONbits.RXM0 = 0; //accept all messages based on filter
-    
     //Use a trick here to keep RXB1 from accepting any messages
     RXB1CONbits.RXM1 = 1; //RXB1 will only accepts extended ID's but the
     RXB1CONbits.RXM0 = 0; //EXIDEN bit in the filter is set to standard so no ID's will be accepted
@@ -109,7 +108,7 @@ void ConfigCAN(void) {
     RXB1CONbits.RXFUL = 0;
     RXB0CONbits.RB0DBEN = 0; //Don't allow overflow to RXB1
 
-    //Switch to operational mode
+    //Switch to operational mode and wait for it
     CANCONbits.REQOP = 0;
     while (CANSTATbits.OPMODE != 0);
 
